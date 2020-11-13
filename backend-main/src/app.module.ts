2a78e97ccb09b1a61df2b4d196810ae8bac839ed
Simon <simon.triem@yahoo.de>
2020-11-13 15:03:14 +0100

    added nest+mongo connection

--- backend-main/src/app.module.ts
+++ backend-main/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirstquestionsModule } from './firstquestions/firstquestions.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FirstquestionsModule],
+  imports: [FirstquestionsModule, MongooseModule.forRoot('mongodb://mongo:27017')],
   controllers: [AppController],
   providers: [AppService],
 })
