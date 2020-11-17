1cdf02a1312ab9e457a660887ee1f1a31c7cf42b
Simon <simon.triem@yahoo.de>
2020-11-17 18:24:16 +0100

    Authentication hinzugefügt + NPM UPDATE

--- backend-main/src/app.module.ts
+++ backend-main/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirstquestionsModule } from './firstquestions/firstquestions.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [FirstquestionsModule, MongooseModule.forRoot('mongodb://root:example@mongo:27017/test?authSource=admin')],
+  imports: [FirstquestionsModule, MongooseModule.forRoot('mongodb://root:example@mongo:27017/test?authSource=admin'), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
