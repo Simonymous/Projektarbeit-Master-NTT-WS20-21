a5d545ff0395d580a6355528907722c5170127e9
Simon <simon.triem@yahoo.de>
2021-01-12 08:17:24 +0100

    logging

--- backend-main/src/moodle/moodle.controller.ts
+++ backend-main/src/moodle/moodle.controller.ts
@@ -1,7 +1,12 @@
 
 import { Controller, Post, Body, Get, Param, Patch, Delete, Res, HttpStatus, UseGuards, Req, Redirect } from '@nestjs/common';
-var uuid = require("uuid4");
-var lti = require("ims-lti");
+/**
+ * 
+ * https://developer.ibm.com/depmodels/cloud/tutorials/implement-learning-tool-interoperability/
+ * 
+ */
+var uuid = require("uuid4"); //Für das speichern der session -> Auch denkbar mit json token?
+var lti = require("ims-lti"); //Modifiziertes ims lti, siehe outcomes.js l206
 
 var a;
 
@@ -13,25 +18,24 @@ export class MoodleController {
   sendGrade(
     @Param ('grade') grade
   ) {
-    console.log("Grade erhalten: "+grade)
-    if(a) {
+    console.log("[LOG] Note erhalten: "+grade)
+    if(a) {                                 //Note muss durch 100 dividiert werden E 0-1
       a.outcome_service.send_replace_result(grade/100, (err, isValid) => {
         if (!isValid) {
           if(!a.outcome_service) {
-            console.log("Kein outcome service vorhanden!")
+            console.log("[LOG] Kein outcome service vorhanden!")
           }
-          console.log("INVALID Grade!!")
+          console.log("[LOG] INVALID Grade:")
           console.log(err)
           
           return "INVALID GRADE "+err
         }
-        console.log("Note erfolgreich übermittelt")
+        console.log("[LOG] Note erfolgreich übermittelt")
         return ('OK')
       })
     } else {
       return "Kein Provider vorhanden!"
     }
-
   }
 
   @Post()
@@ -39,45 +43,25 @@ export class MoodleController {
       @Req() request,
       @Res() response
   ) {
-    var provider = new lti.Provider("top", "secret");
+    var provider = new lti.Provider("top", "secret"); //Shared und public Secret aus moodle
     provider.valid_request(request, (err, isValid) => {
       if (!isValid) {
-        console.log("INVALID")
+        console.log("[LOG] INVALID LTI REQUEST..")
         
-        return "INVALID"+err
+        return "INVALID:"+err
       }
 
-      a = provider;
+      a = provider; //Temporäre Lösung bis die Sesssions in die DB geschoben werden
       
-
-
-
-      // console.log(provider)
-      // console.log("-------------------------")
-      // console.log(sessionID)
-      // console.log("-------------------------")
-      // console.log(sessions)
-      // console.log("-------------------------")
-      // console.log(provider.outcome_service)
-      // provider.outcome_service.send_replace_result(1, (err, isValid) => {
-      //   if (!isValid) {
-      //     console.log("INVALID Grade!!")
-      //     console.log(err)
-          
-      //     return "INVALID"+err
-      //   }
-      //   console.log("Nice")
-      //   return ('OK')
-      // })
-      return "Provider läuft"
+      return "Provider läuft.."
     })
 
-
+    console.log("[LOG] LTI Session initiiert:")
     console.log(a)
 
     //console.log(request)
     //console.log(provider)
-    //return returnString;
+
     //return "OK"
     return response.redirect('http://localhost:8080')
   }
