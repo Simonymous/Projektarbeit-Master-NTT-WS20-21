b183636b6fd65d81fcadb24f9b6ce99f0a2e86a4
Simon <simon.triem@yahoo.de>
2021-01-27 16:56:56 +0100

    testfälle

--- backend-main/src/code-test/codetest.controller.ts
+++ backend-main/src/code-test/codetest.controller.ts
@@ -7,14 +7,19 @@ const testCases = [
        output: 25
     },
     {
-        name:"Add 1 and 1 to be 3..LOL", 
+        name:"Add 1 and 1 to be 2", 
         input: [1,1],
+        output: 2
+     },
+     {
+        name:"Add 1 and 2 to be 3",
+        input: [1,2],
         output: 3
      },
      {
-        name:"ADD 1 and 2 to be 3",
-        input: [1,2],
-        output: 3
+        name:"Add -1 and 2 to be 1",
+        input: [-1,2],
+        output: 1
      },
 ]
 @Controller('codetest')
@@ -29,7 +34,8 @@ export class CodetestController {
         let userFunction = new Function('a','b',code.code)
         testCases.forEach(function(test) {
             let testDescription = "EXPECT "+JSON.stringify(test.input)+" TO BE "+JSON.stringify(test.output)
-            if(userFunction.apply(null,test.input)===test.output) {
+            //console.log(userFunction.apply(null,test.input),"console.log(test); return 2;")
+            if(userFunction.apply('sandbox',test.input)===test.output) {
                 returnArgs.push(testDescription+"...PASSED")
             } else {
                 returnArgs.push(testDescription+"...NOT PASSED")
@@ -39,9 +45,7 @@ export class CodetestController {
           console.log("Funktion konnte nicht erzeugt werden")
       }
       
-      //console.log(myFunction(12,3))
-      //const result = diff(meinCode, code.code);
-      //console.log(result)
+
       return returnArgs;
   }
 }
