45447cae9d63aadb7fb7d8f6a363a61b4bdff31d
Pascal <pn@dataone.de>
2021-03-18 13:23:46 +0100

    Export collection rework

--- frontend-main/src/components/ExerciseManagement/ManageTaskCollection.vue
+++ frontend-main/src/components/ExerciseManagement/ManageTaskCollection.vue
@@ -1,21 +1,17 @@
 <template>
   <div>
-    Title:<InputText
-      type="text"
-      v-model="taskCollection.title"
-      placeholder="Title"
-    />
-    Description:<InputText
+    Title:
+    <InputText type="text" v-model="taskCollection.title" placeholder="Title" />
+    Description:
+    <InputText
       type="text"
       v-model="taskCollection.description"
       placeholder="Description"
     />
-    Tags:<InputText
-      type="text"
-      v-model="taskCollection.tags"
-      placeholder="Tags"
-    />
-    Class:<InputText
+    Tags:
+    <InputText type="text" v-model="taskCollection.tags" placeholder="Tags" />
+    Class:
+    <InputText
       type="text"
       v-model="taskCollection.course"
       placeholder="Class"
@@ -30,7 +26,7 @@
     >
       <Column :rowReorder="true" headerStyle="width: 3rem" />
 
-      <Column field="title" header="Aufgabenname"> </Column>
+      <Column field="title" header="Aufgabenname"></Column>
       <Column
         field="weighting"
         :header="'Gewichtung (Absolut), Gesamt: ' + totalPoints"
@@ -70,30 +66,39 @@
 
     <div class="createTaskCollectionFooter">
       <Button label="Save" v-on:click="handleSaveClick"></Button>
-      <Button label="Delete" v-on:click="handleDeleteClick" class="p-button-danger"></Button>
-      <Button label="Export" v-on:click="handleExportClick" disabled="true"></Button>
+      <Button
+        label="Delete"
+        v-on:click="handleDeleteClick"
+        class="p-button-danger"
+      ></Button>
+      <Button
+        label="Export"
+        v-on:click="handleExportClick"
+        disabled="true"
+      ></Button>
     </div>
   </div>
 </template>
 <script>
-import { ref, watch, onMounted } from "vue";
-import Autocomplete from "./Autocomplete";
-import { useConfirm } from "primevue/useconfirm";
+import { ref, watch, onMounted } from 'vue'
+import Autocomplete from './Autocomplete'
+import { useConfirm } from 'primevue/useconfirm'
+import { useToast } from 'primevue/usetoast'
 
-import { getBackendRequestDummy } from "../../helper/dummyRequests";
+import { getBackendRequestDummy } from '../../helper/dummyRequests'
 import {
   getBackendRequest,
   postBackendRequest,
   deleteBackendRequest,
   putBackendRequest,
-} from "../../helper/requests";
-const PATHS = require("../../../config.json").URL_PATHS;
+} from '../../helper/requests'
+const PATHS = require('../../../config.json').URL_PATHS
 
 const TASK_PATH = PATHS.TASK_PATH
-const TASK_COLLECTION_PATH = PATHS.TASK_COLLECTION_PATH;
-const CREATE_TASK_COLLECTION_PATH = PATHS.CREATE_TASK_COLLECTION_PATH;
-const UPDATE_TASK_COLLECTION_PATH = PATHS.UPDATE_TASK_COLLECTION_PATH;
-const DELETE_TASK_COLLECTION_PATH = PATHS.DELETE_TASK_COLLECTION_PATH;
+const TASK_COLLECTION_PATH = PATHS.TASK_COLLECTION_PATH
+const CREATE_TASK_COLLECTION_PATH = PATHS.CREATE_TASK_COLLECTION_PATH
+const UPDATE_TASK_COLLECTION_PATH = PATHS.UPDATE_TASK_COLLECTION_PATH
+const DELETE_TASK_COLLECTION_PATH = PATHS.DELETE_TASK_COLLECTION_PATH
 const DEEP_EXPORT_TASK_COLLECTION = PATHS.DEEP_EXPORT_TASK_COLLECTION
 
 export default {
@@ -104,117 +109,131 @@ export default {
     taskCollectionID: String,
   },
   setup(props, { emit }) {
-    const totalPoints = ref();
-    const confirm = useConfirm();
+    const totalPoints = ref()
+    const confirm = useConfirm()
+    const toast = useToast()
 
     let emptyTaskCollection = {
       _id: -1,
-      type: "TaskCollection",
-      title: "",
-      description: "",
+      type: 'TaskCollection',
+      title: '',
+      description: '',
       date: new Date(),
       tags: [],
-      course: "",
-      creator: "",
+      course: '',
+      creator: '',
       tasks: [],
-    };
+    }
 
-    const taskCollection = ref({ ...emptyTaskCollection });
+    const taskCollection = ref({ ...emptyTaskCollection })
 
-    init();
+    init()
     async function init() {
       if (props.taskCollectionID === -1) {
-        taskCollection.value = { ...emptyTaskCollection };
+        taskCollection.value = { ...emptyTaskCollection }
       } else {
-        await getTaskCollections();
+        await getTaskCollections()
       }
-      calcTotalPoints();
+      calcTotalPoints()
     }
 
     watch(
       () => props.taskCollectionID,
-      () => init()
-    );
+      () => init(),
+    )
 
     async function getTaskCollections() {
       try {
-        if (process.env.VUE_APP_BACKEND_ONLINE === "true") {
+        if (process.env.VUE_APP_BACKEND_ONLINE === 'true') {
           taskCollection.value = await getBackendRequest(
-            TASK_COLLECTION_PATH + "/" + props.taskCollectionID
-          );
+            TASK_COLLECTION_PATH + '/' + props.taskCollectionID,
+          )
         } else {
           taskCollection.value = getBackendRequestDummy(
-            TASK_COLLECTION_PATH + "/" + props.taskCollectionID
-          );
+            TASK_COLLECTION_PATH + '/' + props.taskCollectionID,
+          )
         }
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
     }
 
     async function handleSaveClick() {
       try {
         if (taskCollection.value._id === -1) {
-          postBackendRequest(CREATE_TASK_COLLECTION_PATH, taskCollection.value);
+          postBackendRequest(CREATE_TASK_COLLECTION_PATH, taskCollection.value)
         } else {
-          putBackendRequest(UPDATE_TASK_COLLECTION_PATH, taskCollection.value);
+          putBackendRequest(UPDATE_TASK_COLLECTION_PATH, taskCollection.value)
         }
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
     }
 
     async function handleDeleteClick() {
       try {
         confirm.require({
-          message: "Möchten sie das Aufgabenblatt löschen?",
-          header: "Aufgabenblatt löschen",
-          icon: "pi pi-exclamation-triangle",
+          message: 'Möchten sie das Aufgabenblatt löschen?',
+          header: 'Aufgabenblatt löschen',
+          icon: 'pi pi-exclamation-triangle',
           accept: () => {
             deleteBackendRequest(
-              DELETE_TASK_COLLECTION_PATH + "/" + taskCollection.value._id
-            );
-            taskCollection.value = { ...emptyTaskCollection };
+              DELETE_TASK_COLLECTION_PATH + '/' + taskCollection.value._id,
+            )
+            taskCollection.value = { ...emptyTaskCollection }
           },
-        });
+        })
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
     }
 
     function calcTotalPoints() {
-      totalPoints.value = 0;
+      totalPoints.value = 0
       taskCollection.value.tasks.forEach((task) => {
-        totalPoints.value += +task.weighting;
-      });
+        totalPoints.value += +task.weighting
+      })
     }
 
     function onRowReorder(event) {
-      taskCollection.value.tasks = event.value;
+      taskCollection.value.tasks = event.value
     }
 
     function handleRemoveTask(event) {
-      console.log(event);
-      taskCollection.value.tasks.splice(event.index, 1);
+      console.log(event)
+      taskCollection.value.tasks.splice(event.index, 1)
     }
 
     async function handleExportClick(event) {
-      let exportTaskCollection = taskCollection.value
-      exportTaskCollection = exportTaskCollection.tasks.slice(0,exportTaskCollection.tasks.length) 
-      taskCollection.value.tasks.forEach(async task => exportTaskCollection.push( await getBackendRequest(TASK_PATH + '/' + task._id)))
-      //let exportTaskCollection = await getBackendRequest(DEEP_EXPORT_TASK_COLLECTION + '/' + taskCollection.value.id)
-      const data = JSON.stringify(exportTaskCollection);
-      const blob = new Blob([data], { type: "application/json" });
-      const link = document.createElement("a");
-      link.href = URL.createObjectURL(blob);
-      link.download =
-        exportTaskCollection.title + "-" + exportTaskCollection._id;
-      link.click();
-      URL.revokeObjectURL(link.href);
+      try {
+        let exportCollection = [taskCollection.value]
+        for (let i = 0; i < taskCollection.value.tasks.length; i++) {
+          exportCollection.push(
+            await getBackendRequest(
+              TASK_PATH + '/' + taskCollection.value.tasks[i]._id,
+            ),
+          )
+        }
+
+        const data = JSON.stringify(exportCollection)
+        const blob = new Blob([data], { type: 'application/json' })
+        const link = document.createElement('a')
+        link.href = URL.createObjectURL(blob)
+        link.download =
+          exportCollection[0].title + '-' + exportCollection[0]._id
+        link.click()
+        URL.revokeObjectURL(link.href)
+      } catch (e) {
+        toast.add({
+          severity: 'error',
+          summary: e.message,
+          life: 10000,
+        })
+      }
     }
 
     function handleInspectTask(item) {
-      emit("exerciseSelected", { id: item.data._id, kindOfExercise: "task" });
+      emit('exerciseSelected', { id: item.data._id, kindOfExercise: 'task' })
     }
 
     function addTask(payload) {
@@ -222,8 +241,8 @@ export default {
         title: payload.title,
         _id: payload._id,
         weighting: 0,
-      });
-      console.log(payload);
+      })
+      console.log(payload)
     }
 
     return {
@@ -237,9 +256,9 @@ export default {
       handleRemoveTask,
       handleInspectTask,
       handleExportClick,
-    };
+    }
   },
-};
+}
 </script>
 <style lang="css">
 .pointsIncorrect {
