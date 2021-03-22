7a573107465617c7e0ffe2c2e5645130e5dbbcd5
Grossdrache <philipp@tull.de>
2021-03-22 14:58:27 +0100

    Import implementiert

--- frontend-main/src/components/ExerciseManagement/ImportExport.vue
+++ frontend-main/src/components/ExerciseManagement/ImportExport.vue
@@ -5,25 +5,24 @@
       :customUpload="true"
       @uploader="myUploader"
       accept=".json"
-      :multiple="true"
       :auto="true"
-      @remove="test()"
-      @upload="test()"
-      @clear="test()"
     />
     <Button
       label="Task(s) neu erstellen"
       v-on:click="handleCreateNewTasks"
-    ></Button>
-    <Button
-      label="Task(s) überschreiben"
-      v-on:click="handleOverwriteTasks"
     ></Button>
   </div>
 </template>
 <script>
 import { ref } from "vue";
 import { useToast } from "primevue/usetoast";
+
+const PATHS = require("../../../config.json").URL_PATHS;
+
+const CREATE_TASK_PATH = PATHS.CREATE_TASK_PATH;
+const CREATE_TASK_COLLECTION_PATH = PATHS.CREATE_TASK_COLLECTION_PATH;
+
+import { postBackendRequest } from "../../helper/requests";
 
 export default {
   name: "importExport",
@@ -38,33 +37,94 @@ export default {
         const reader = new FileReader();
         reader.onload = (e) => myFiles.value.push(JSON.parse(e.target.result));
         reader.readAsText(file);
-        console.log(myFiles.value)
-      });
-      toast.add({
-        severity: "success",
-        summary: "TaskCollection/Task wurde importiert",
-        life: 10000,
+        console.log(myFiles.value);
       });
     }
 
-    function test() {
-      console.log("oksdnm");
-      // console.log(event)
+    async function importFile(filecontent) {
+      try {
+        let taskCollections = [];
+
+        filecontent.forEach((exercise) => {
+          console.log(exercise);
+          if (exercise.type === "taskCollection") {
+            taskCollections.push(exercise);
+            exercise.tasks.forEach((task) => {
+              if (
+                !filecontent.some(
+                  (innerExercise) => innerExercise._id == task._id
+                )
+              ) {
+                throw "Missing Tasks " + task._id;
+              }
+            });
+            exercise.tasks.includes;
+          } else if (exercise.type !== "taskCollection") {
+            throw "Unknown Element";
+          }
+        });
+
+        let mapping = [];
+        for (let i = 0; i < filecontent.length; i++) {
+          if (filecontent[i].type === "task") {
+            let response = await postBackendRequest(CREATE_TASK_PATH, {
+              ...filecontent[i],
+            });
+            mapping.push({
+              oldID: filecontent[i]._id,
+              newID: response.data._id,
+            });
+          }
+        }
+
+        taskCollections.forEach((taskCollection) => {
+          taskCollection.tasks.forEach((task, taskIndex) => {
+            let newIndex = mapping.find(
+              (mappingElement) => mappingElement.oldID === task._id
+            ).newID;
+            taskCollection.tasks[taskIndex] = { ...task, _id: newIndex };
+          });
+          postBackendRequest(CREATE_TASK_COLLECTION_PATH, taskCollection).then(
+            (response) => {
+              toast.add({
+                severity: "success",
+                summary:
+                  "TaskCollection " +
+                  response.payload.title +
+                  " wurde gespeichert",
+                life: 10000,
+              });
+            }
+          );
+        });
+      } catch (error) {
+        toast.add({
+          severity: "error",
+          summary: "Error creating Exercises:",
+          detail: error,
+          life: 10000,
+        });
+      }
     }
 
     function handleCreateNewTasks() {
       console.log(myFiles.value);
+      myFiles.value.forEach((file) => {
+        importFile(file);
+      });
     }
 
-    function handleOverwriteTasks() {}
     return {
       myUploader,
       handleCreateNewTasks,
-      handleOverwriteTasks,
-      test,
     };
   },
 };
 </script>
+<style lang="scss" scoped>
+::v-deep(.p-button-icon-only) {
+  visibility: hidden;
+}
+</style>
 
 
